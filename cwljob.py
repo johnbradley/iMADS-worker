fbdd49743864f69a042d932c97b1145024af8eae
Dan Leehr <dan.leehr@duke.edu>
2016-08-09 16:34:58 -0400

    Adds writing JSON and model matrixing

--- cwljob.py
+++ cwljob.py
@@ -1,19 +1,25 @@
+import json
+
 def make_file_dict(path):
   return {'class': 'File', 'path': path}
 
 class CwlJobGenerator:
   def __init__(self, config_dict):
     self.config_dict = config_dict
+    self._job = None
 
-  def generate_job(self):
-    job = dict.copy(self.config_dict)
-    # If single model and multiple cores, expand models
-    model_filenames = list(job['model_filenames'])
-    num_models, num_cores = len(model_filenames), len(job['cores'])
-    if num_cores > num_models:
-      model_filenames = model_filenames * num_cores
-    job['models'] = [make_file_dict(model) for model in model_filenames]
-    return job
+  @property
+  def job(self):
+    if self._job is None:
+      job = dict.copy(self.config_dict)
+      # If single model and multiple cores, expand models
+      model_filenames = list(job['model_filenames'])
+      num_models, num_cores = len(model_filenames), len(job['cores'])
+      if num_cores > num_models:
+        model_filenames = model_filenames * num_cores
+      job['models'] = [make_file_dict(model) for model in model_filenames]
+      self._job = job
+    return self._job
 
-
-
+  def write_json(self, output_file):
+    json.dump(self.job, output_file)
--- tests/test_cwljob.py
+++ tests/test_cwljob.py
@@ -1,12 +1,26 @@
 from unittest import TestCase
 from cwljob import CwlJobGenerator
 import test_data
+import tempfile
 
 class CwlJobGeneratorTestCase(TestCase):
 
   def tests_model_dict(self):
     g = CwlJobGenerator(test_data.CONFIG_2X2)
-    j = g.generate_job()
-    self.assertIsNotNone(j)
+    self.assertIsNotNone(g.job)
     self.assertNotIn('models', test_data.CONFIG_2X2, 'test dataset should have model_filenames')
-    self.assertIn('models', j, 'generated job dict should just have models')
+    self.assertIn('models', g.job, 'generated job dict should just have models')
+
+  def test_writes_json(self):
+    g = CwlJobGenerator(test_data.CONFIG_2X2)
+    output_file = tempfile.NamedTemporaryFile()
+    self.assertEqual(output_file.tell(), 0)
+    g.write_json(output_file)
+    self.assertNotEqual(output_file.tell(), 0, 'write_json should have written to the file')
+
+  def test_matrixes_models(self):
+    g = CwlJobGenerator(test_data.CONFIG_1X3)
+    self.assertEqual(len(test_data.CONFIG_1X3['model_filenames']), 1)
+    self.assertEqual(len(test_data.CONFIG_1X3['cores']), 3)
+    self.assertEqual(len(g.job['models']), 3)
+    self.assertEqual(len(g.job['cores']), 3)

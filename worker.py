06e5a377c68118e9102fcfa993ebdab49968d1bc
Dan Leehr <dan.leehr@duke.edu>
2016-10-21 14:51:47 -0400

    Updates worker to look for _vs_ and use preference instead

--- worker.py
+++ worker.py
@@ -1,6 +1,5 @@
 from predict_service.runner import PredictionRunner
 
-
 class WorkerException(Exception):
     pass
 
@@ -15,12 +14,16 @@ class PredictionsWorker(object):
         return result.get('path')
 
     def run(self, sequence_file, model_identifier):
-        # TODO: Check model identifier for vs and run preferences instead
-        workflow = PredictionRunner.predict_workflow()
-        runner = PredictionRunner(workflow,
-                                  sequence_file,
+        if '_vs_' in model_identifier:
+            strategy = PredictionRunner.strategy_preference
+            config_file = self.config.preferences_config_file
+        else:
+            strategy = PredictionRunner.strategy_predict
+            config_file = self.config.predictions_config_file
+        runner = PredictionRunner(sequence_file,
                                   model_identifier,
-                                  self.config.config_file,
+                                  config_file,
                                   self.config.model_files_dir,
-                                  self.config.output_dir)
+                                  self.config.output_dir,
+                                  strategy)
         return self.extract_predictions(runner.run())
